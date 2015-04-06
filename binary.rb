c07a1ee456f032f0c668da3f1822133b01fba4fb
Tony DiNitto <think41c@gmail.com>
2015-04-06 00:13:27 -0600

    Basic logic of converting from binary implemented.

--- binary.rb
+++ binary.rb
@@ -5,7 +5,18 @@ class Binary
 
 
   def to_decimal
-    1
+    power = 0
+    full_number = 0 
+
+    @x.reverse.each_char do |x|
+      spot_number = x.to_i**power
+      full_number += spot_number
+      power += 1 
+    end
+    full_number
   end
 
 end
+
+a = Binary.new("5")
+p a.to_decimal
