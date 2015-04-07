27274883be142eff42e2112a0c0cc8ccbe377bdd
Tony DiNitto <think41c@gmail.com>
2015-04-06 21:35:40 -0600

    All tests passing. Removed magic numbers.

--- binary.rb
+++ binary.rb
@@ -5,13 +5,14 @@ class Binary
 
   def to_decimal
     if @x.each_char.any? { |x| x != "1" && x != "0"}
-      return 0 
+      full_number = 0 
     else
       power = 0
       full_number = 0 
+      base = 2 
 
       @x.reverse.each_char do |x|
-        spot_number = x.to_i * 2.to_i**power
+        spot_number = x.to_i * base.to_i**power
         full_number += spot_number
         power += 1
       end
@@ -19,6 +20,3 @@ class Binary
     full_number
   end
 end
-
-a = Binary.new("carrot123").to_decimal
-puts a
